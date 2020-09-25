c9782fe161701d915fad02da03568d5fca71df53
boramcom <boramcom@daum.net>
2020-09-25 16:28:37 +0900

    tag라이브러리 에러 나와서 pom에 추가

--- src/main/java/org/edu/service/MemberServiceImpl.java
+++ src/main/java/org/edu/service/MemberServiceImpl.java
@@ -54,10 +54,10 @@ public class MemberServiceImpl implements IF_MemberService {
 		System.out.println(list);
 		//6개월동안 회원정보 수정을 안한 회원을 휴면 계정으로 업데이트(아래)
 		System.out.println("===========6개월간 회원정보수정 안한 회원 휴면계정으로 처리");
-		for(MemberVO vo:list) {
+		/*for(MemberVO vo:list) {
 			vo.setEnabled(false);//휴면계정으로 SET변경
 			memberDAO.updateMember(vo);
-		}
+		}*/
 		return memberDAO.testJobMethod();
 	}
 
